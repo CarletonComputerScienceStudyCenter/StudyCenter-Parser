375c02ee84ec53a6c9655863a94946e85b08c3fa
MathyouMB <matthewmacraebovell@bell.net>
2020-03-14 01:24:11 -0400

    better default seed

--- seeds.rb
+++ seeds.rb
@@ -1,6 +1,13 @@
+2804 = Course.create(
+    title: 'Discrete Structures II',
+    code: 'COMP 2804',
+    description: 'A second course in discrete mathematics and discrete structures. Topics include: counting, sequences and sums, discrete probability, basic statistics, recurrence relations, randomized algorithms. Material is illustrated through examples from computing.'
+)
+
 midterm = Quiz.create(
     title: "Midterm Fall 2018",
     description: "This is a the midterm from Fall 2018.",
+    course_id: 2804.id
 )
 
 # -----
